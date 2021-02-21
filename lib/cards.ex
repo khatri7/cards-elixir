1fcc47c3aa42366e16f90475f69a3c26762cc4ba
Abhishek Khatri <abhishekkhatri@Abhisheks-MacBook-Pro.local>
2021-02-21 14:57:18 +0530

    first commit

---
+++ lib/cards.ex
@@ -0,0 +1,34 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    for value <- values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+end
