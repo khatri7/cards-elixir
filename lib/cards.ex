703db360824ac72aec700854471962c53ad0ff1f
Abhishek Khatri <abhishekkhatri@Abhisheks-MacBook-Pro.local>
2021-02-21 16:31:39 +0530

    case and pattern matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -16,7 +16,7 @@ defmodule Cards do
     :world
   end
 
-  def create_deck do
+  def createDeck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
     for value <- values, suit <- suits do
@@ -30,5 +30,26 @@ defmodule Cards do
 
   def contains?(deck, card) do
     Enum.member?(deck, card)
+  end
+
+  def hand({hand, _restOfDeck}) do
+    hand
+  end
+
+  def deal(deck, handSize) do
+    Enum.split(deck, handSize)
+    |> hand
+  end
+
+  def save(deck, fileName) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(fileName, binary)
+  end
+
+  def load(fileName) do
+    case File.read(fileName) do
+      {:ok, binary}-> :erlang.binary_to_term binary
+      {:error, _reason} -> "No such file found!"
+    end
   end
 end
